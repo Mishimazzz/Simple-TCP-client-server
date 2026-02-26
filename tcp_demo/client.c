4d7fdeb6f1893a30ea5ad9d5128c7c4404232639
yying8 <yueyan.ying0256@gmail.com>
2026-02-26 15:43:37 -0500

    update

---
+++ tcp_demo/client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[])
+{
+  char *ip = "127.0.0.1";
+  int port = 8080;
+
+  if (argc >= 2)
+    ip = argv[1];
+  if (argc >= 3)
+    port = atoi(argv[2]);
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0)
+  {
+    perror("socket");
+    return 1;
+  }
+
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  inet_pton(AF_INET, ip, &server_addr.sin_addr);
+
+  if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+  {
+    perror("connect");
+    return 1;
+  }
+
+  char *msg = "hello from client\n";
+  send(sock, msg, strlen(msg), 0);
+
+  char buffer[1024];
+  int n = recv(sock, buffer, sizeof(buffer) - 1, 0);
+  if (n > 0)
+  {
+    buffer[n] = '\0';
+    printf("Server replied: %s", buffer);
+  }
+
+  close(sock);
+  return 0;
+}
--- tcp_demo/server.c
+++ tcp_demo/server.c
@@ -1,7 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>// for linux
-#include <arpa/inet.h>// for ip address transfer
-#include <sys/socket.h>// for socket api
+#include <unistd.h>// for Linux system
+#include <arpa/inet.h>// for ip address
+#include <sys/socket.h> // socket API
 
+int main(int argc, char *argv[])
+{
+  //setting port
+  int port = 8080;
+  if (argc >= 2) port = atoi(argv[1]);
+
+  // create socket, Creating socket file descriptor
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0)
+  {
+    perror("socket failed");
+    return 1;
+  }
+
+  // Forcefully attaching socket to the port 8080
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(port);
+
+  //0.0.0.0:port
+  if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+  {
+    perror("bind failed");
+    return 1;
+  }
+
+  if (listen(server_fd, 5) < 0)
+  {
+    perror("listen failed");
+    return 1;
+  }
+
+  printf("Server listening on port %d...\n", port);
+
+  struct sockaddr_in client_addr;
+  socklen_t len = sizeof(client_addr);
+
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
+  if (client_fd < 0)
+  {
+    perror("client accept failed");
+    return 1;
+  }
+
+  printf("Client connected\n");
+
+  char buffer[1024];
+
+  while (1)
+  {
+    int n = recv(client_fd, buffer, sizeof(buffer), 0);
+    if (n <= 0)
+      break;
+
+    send(client_fd, buffer, n, 0); // echo back
+  }
+
+  close(client_fd);
+  close(server_fd);
+  return 0;
+}
