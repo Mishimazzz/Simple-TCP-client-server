d45ffe56ac61d4a0ffd11c42b20dc1803c736c52
Yueyan Ying <yueyan.ying0256@gmail.com>
2026-02-28 15:20:39 -0500

    finish

--- tcp_demo/client.c
+++ tcp_demo/client.c
@@ -7,44 +7,52 @@
 
 int main(int argc, char *argv[])
 {
-  char *ip = "127.0.0.1";
-  int port = 8080;
+    char *ip = "127.0.0.1";
+    int port = 8080;
 
-  if (argc >= 2)
-    ip = argv[1];
-  if (argc >= 3)
-    port = atoi(argv[2]);
+    if (argc >= 2) ip = argv[1];
+    if (argc >= 3) port = atoi(argv[2]);
 
-  //create socket
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0)
-  {
-    perror("socket");
-    return 1;
-  }
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
 
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(port);
-  inet_pton(AF_INET, ip, &server_addr.sin_addr);
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &server_addr.sin_addr);
 
-  if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-  {
-    perror("Connection Failed");
-    return 1;
-  }
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect");
+        return 1;
+    }
 
-  char *msg = "hello from client\n";
-  send(sock, msg, strlen(msg), 0);
+    char buffer[1024];
+    int count = 1;
 
-  char buffer[1024];
-  int n = recv(sock, buffer, sizeof(buffer) - 1, 0);
-  if (n > 0)
-  {
-    buffer[n] = '\0';
-    printf("Server replied: %s", buffer);
-  }
+    while (1)
+    {
+        // create message
+        sprintf(buffer, "message %d\n", count);
 
-  close(sock);
-  return 0;
+        // send
+        send(sock, buffer, strlen(buffer), 0);
+        printf("send: %s", buffer);
+
+        // receive echo
+        int n = recv(sock, buffer, sizeof(buffer)-1, 0);
+        if (n <= 0) break;
+
+        buffer[n] = '\0';
+        printf("reply: %s", buffer);
+
+        count++;
+        sleep(1);
+    }
+
+    close(sock);
+    return 0;
 }
